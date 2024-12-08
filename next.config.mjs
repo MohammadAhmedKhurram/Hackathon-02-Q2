a8c2a64e75882cd3fb13a399de6b347894375c3d
MohammadAhmedKhurram <162457856+MohammadAhmedKhurram@users.noreply.github.com>
2024-12-09 02:01:08 +0500

    --

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    eslintIgnore: [
+      "**/app/components/**/*.ts",
+      "**/*.test.tsx",
+    ],
+  };
+  
+  module.exports = nextConfig;
+  
